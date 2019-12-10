c3af58fe3aad5c614d662632dda55cd721b551c5
Berkan <berkansivri@gmail.com>
2019-12-10 22:46:46 +0300

    try

--- solution.js
+++ solution.js
@@ -1,46 +1,54 @@
-const CryptoJS = require('crypto-js')
+const { MD5 } = require('crypto-js')
+const { permutation } = require('generatorics')
 
-const solve = () => {
+const solve = (anagram) => {
   const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('wordlist')
   })
 
-  const foundWords = []
-  // const str = ["poultry", "outwits", "ants"].map(s => s.split('').sort().join('')).join()
-  // const str = "poultry outwits ants".split('').sort().join('')
+  const unspaceAnagram =  anagram.replace(/ /g, "")
 
-  const str = "poultryoutwitsants".split('')
+  const chars = {}
+  for(let char of unspaceAnagram) {
+    chars[char] = (chars[char] || 0) + 1
+  }
+  
+  const foundWords = []
+
   lineReader.on('line', (line) => {
-    // let sortedLine = line.toString().split('').sort().join('')
-    // if (arr.some(s => s.split('').sort().join('') === sortedLine)) {
-    line = line.toString().split('')
-    if (line.every(s => str.includes(s) && (line.filter(l => l === s).length <= str.filter(x => x === s).length)))
-      foundWords.push(line.join(''))
-    // if (str.includes(sortedLine))
-    // }
+    const lineArr = line.toString().split('')
+    const letterChars = {}
+    
+    lineArr.forEach(l => letterChars[l] = (letterChars[l] || 0) + 1)
+
+    const isValid = lineArr.every(l => chars[l] && (letterChars[l] <= chars[l]))
+    if(isValid) {
+      foundWords.push(lineArr.join(''))
+    }
   }).on('close', () => {
-    combineWords([...new Set(foundWords)], [])
-    // combineWords([...new Set(foundWords)], [])
-    // console.log([...new Set(foundWords)].length)
+    const wordList = [...new Set(foundWords)].slice(0, 5)
+    // combineWords(wordList, [], wordList.length, 0)
+    // combineWords([1,2,3], [], 3, 0)
+    permutation(wordList, 3).
   })
 }
-// before anagram check. maybe not sort?
-combineWords = (words, stack) => {
+
+combineWords = (words, stack, len, i) => {
   if (stack.length === 3) {
+    console.log(stack.join(''));
     const secret = stack.join(' ')
-    const hash = CryptoJS.MD5(secret).toString()
-    if (hash === "e4820b45d2277f3844eac66c903e84be") {
-      console.log("basic:", secret);
+    stack = []
+    // const hash = MD5(secret).toString()
+    // if (hash === "e4820b45d2277f3844eac66c903e84be") {
+    //   console.log("basic:", secret);
+    // }
+  } 
+    for (; i < len; i++) {
+      stack.push(words[i])
+      combineWords(words, stack, len, i+1)
+      i-=2
+      stack.pop()
     }
-  } else {
-    for (let w of words) {
-      if (!stack.includes(w)) {
-        stack.push(w)
-        combineWords(words, stack)
-        stack.pop()
-      }
-    }
-  }
 }
 
-solve();
+solve("poultry outwits ants")
