f5de3b77eab385421f340b8687a06be851c71893
berkansivri <berkansivri@gmail.com>
2019-12-12 17:14:45 +0300

    5 -> 4

--- solution.js
+++ solution.js
@@ -1,10 +1,14 @@
 const MD5 = require('crypto-js/md5')
 
+//    Easy secret phrase: printout stout yawls
+//    Medium secret phrase: ty outlaws printouts
+//    Hard secret phrase: wu lisp not statutory
 const hashValues = [
   "e4820b45d2277f3844eac66c903e84be", // Easy
   "23170acc097c24edb98fc5488ab033fe", // Medium
   "665e5bcb0c20062fe8abaaf4628bb154" // Hard
 ]
+const chars = {}
 
 const solve = (anagram) => {
   const lineReader = require('readline').createInterface({
@@ -13,7 +17,6 @@ const solve = (anagram) => {
 
   const unspaceAnagram = anagram.replace(/ /g, "")
 
-  const chars = {}
   for (let char of unspaceAnagram) {
     chars[char] = (chars[char] || 0) + 1
   }
@@ -32,15 +35,16 @@ const solve = (anagram) => {
     }
   }).on('close', () => {
     const wordList = [...new Set(foundWords.map(w => JSON.stringify(w)))].map(w => JSON.parse(w))
-
+    console.time("solution")
     for (let words of permutation(wordList, 3, chars)) {
       console.log(words);
       const md5Str = MD5(words).toString()
       if (hashValues.indexOf(md5Str) > -1) {
+        console.timeEnd("solution");
         console.log(words);
+        console.time("solution");
       }
     }
-    console.log("----finish----");
   })
 }
 
@@ -51,63 +55,45 @@ function getChars(obj, word) {
   return obj
 }
 
-function checkCharCounts([char, count]) {
-  return this[char] === count 
+function checkCharCounts(word) {
+  for (let c of word) {
+    this[c] = (this[c] || 0) + 1
+    if (!(this[c] <= chars[c])) return false
+  }
+  return true
 }
 
 function* permutation(arr, size, anagramChars) {
-  let len = arr.length
+  let datasetLen = arr.length
   let data = []
   let used = []
-  let anagramCharEntries = Object.entries(anagramChars)
   let anagramLength = Object.values(anagramChars).reduce((a, c) => a + c)
-  let charCounts = {}
   let isValid = true
-  // let anagramChars = {...chars}
-  // let arrChars = {}
-  // let isValid = true
-  console.time("permutation")
   yield* permute(0)
-  
-  function* permute(index, dataLen = 0) {
+
+  function* permute(index, len = 0) {
     if (index === size) {
-      console.log(data.join(' '));
-      if (anagramLength === dataLen) {
-        let dataChars = data.reduce(getChars, {})
-        isValid = anagramCharEntries.every(checkCharCounts.bind(dataChars))
-        if (isValid) {
-          console.timeEnd("permutation")
-          console.time("permutation")
-          return yield data.join(' ')
-        }
-      }
+      if (isValid) return yield data.join(' ')
       return
     }
-    for (let i = 0; i < len; i++) {
+    for (let i = 0; i < datasetLen; i++) {
       if (!used[i]) {
-        isValid = true
-        charCounts = {}
-        data[index] = arr[i]
-        if(data.length === 3) {
-          for(let word of data) {
-            for (let l of word) {
-              charCounts[l] = (charCounts[l] || 0) + 1
-              if(charCounts[l] > anagramChars[l]) {
-                isValid = false
-                break
-              }
-            }
-            if(!isValid) break
-          }
-          if(!isValid) {
-            index--
-            continue
-          }
-        } 
         
+        isValid = true
+        let wordLength = len + arr[i].length
+
+        if ((wordLength === anagramLength)) {
+          charCounts = {}
+          let tempData = [...data]
+          tempData[index] = arr[i]
+
+          isValid = tempData.every(checkCharCounts.bind(charCounts))
+          if (!isValid) continue
+        } else isValid = false
         
         used[i] = true
-        yield* permute(index + 1, dataLen + data[index].length)
+        data[index] = arr[i]
+        yield* permute(index + 1, wordLength)
         used[i] = false
       }
     }
@@ -115,3 +101,4 @@ function* permutation(arr, size, anagramChars) {
 }
 
 solve("poultry outwits ants")
+// solve("abc")
