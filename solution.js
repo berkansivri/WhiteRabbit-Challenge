d2ea31dbd1ecb309d4986522a58591877b6184b7
Berkan <berkansivri@gmail.com>
2019-12-14 22:58:40 +0300

    really fast easy&medium with recursion

--- solution.js
+++ solution.js
@@ -9,7 +9,8 @@ const MD5 = require('md5')
 const hashValues = [
   "e4820b45d2277f3844eac66c903e84be", // Easy
   "23170acc097c24edb98fc5488ab033fe", // Medium
-  "665e5bcb0c20062fe8abaaf4628bb154" // Hard
+  "4624d200580677270a54ccff86b9610e", // From web //
+  "665e5bcb0c20062fe8abaaf4628bb154", // Hard
 ]
 let anagramChars = {}
 
@@ -61,18 +62,15 @@ function addCharCounts(word, chars = {}) {
 }
 
 function removeCharCounts(word, chars) {
-  for (let c of word) {
+  for (let c of word || "") {
     chars[c] && chars[c]--
   }
 }
 
 function findPhrases(wordList) {
-  let size = 3
 
   while (hashValues.length > 0) {
-
-    console.log(`--- Checking with ${size} word ---`)
-    for (let words of listFilter(wordList, size)) {
+    for (let words of listFilter(wordList)) {
       const md5String = MD5(words).toString()
       const index = hashValues.indexOf(md5String)
 
@@ -81,13 +79,12 @@ function findPhrases(wordList) {
         hashValues.splice(index, 1)
       }
     }
-    size++
   }
 }
 
-function* listFilter(arr, size) {
-  const phrase = new Array(size).fill(''),
-    anagramLength = Object.values(anagramChars).reduce((a, c) => a + c)
+function* listFilter2(arr, size) {
+  const phrase = []
+  anagramLength = Object.values(anagramChars).reduce((a, c) => a + c)
   let filteredArr = [],
     chars = {},
     len = 0
@@ -119,14 +116,51 @@ function* listFilter(arr, size) {
   }
 }
 
+function* listFilter(arr) {
+  let phrase = []
+    anagramLength = Object.values(anagramChars).reduce((a, c) => a + c)
+  let filteredArr,
+    chars = {},
+    len = 0,
+    wordCount = 1
+
+  function* fn(index) {
+    if (index === wordCount - 1) {
+      len = Object.values(chars).reduce((a, c) => a + c, 0)
+      filteredArr = arr.filter(word => word.length === (anagramLength - len) && isValidWord(word, { ...chars }))
+      if(filteredArr.length > 0) {
+        for (let k = 0; k < filteredArr.length; k++) {
+          phrase[index] = filteredArr[k]
+          return yield* permutation(phrase)
+        }
+      }
+      return
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+      removeCharCounts(phrase[index], chars)
+      phrase[index] = ""
+      if (!addCharCounts(arr[i], chars)) continue
+      phrase[index] = arr[i]
+      yield* fn(index + 1)
+    }
+  }
+
+  for (; wordCount <= arr.length; wordCount++) {
+    console.log(`--- Checking with ${wordCount} word ---`)
+    yield* fn(0)
+  }
+
+}
+
 
 function* permutation(arr, size = arr.length) {
   const data = [],
     used = [],
     len = arr.length
-  yield* permutationUtil(0)
+  yield* perm(0)
 
-  function* permutationUtil(index) {
+  function* perm(index) {
     if (index === size)
       return yield data.join(' ')
 
@@ -134,7 +168,7 @@ function* permutation(arr, size = arr.length) {
       if (!used[i]) {
         used[i] = true
         data[index] = arr[i]
-        yield* permutationUtil(index + 1)
+        yield* perm(index + 1)
         used[i] = false
       }
     }
@@ -142,9 +176,8 @@ function* permutation(arr, size = arr.length) {
 }
 
 function showResult(hash, phrase) {
-  console.timeEnd("found in")
+  console.timeLog("found in")
   console.log(`${hash} : ${phrase}`)
-  console.time("found in")
 }
 
 main("wordlist", "poultry outwits ants")
