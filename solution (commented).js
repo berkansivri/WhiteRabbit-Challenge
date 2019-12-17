85ea52508bdd4b646b63b5de66bc45f4b7243f1c
berkansivri <berkansivri@gmail.com>
2019-12-17 17:19:34 +0300

    commented file start

---
+++ solution (commented).js
@@ -0,0 +1,172 @@
+//    Challenge link: https://followthewhiterabbit.trustpilot.com/cs/step3.html
+
+//                                                      Approximately Found Times
+//    Easy secret phrase:     printout stout yawls            2.6   second
+//    Medium secret phrase:   ty outlaws printouts            1.3   second
+//    Hard secret phrase:     wu lisp not statutory           3.20  minute
+
+/**
+ * --- Ideas about algorithm ---,
+ * Keep the character set of anagraf of the phrase and hashes.
+ * Read the file (100k word) line by line and after checking strings with anagram of phrase populate an array.
+ * Remove dublicates (1659 left) of the array and start to try crush hashes.
+ * Start with iterate over wordlist to get a word and check conditions.
+ *    Firstly check the length of the phrase (faster).
+ *    Then check for the each character of the phrase with anagram characters.
+ * If the phrase is valid, get permutation of the phrase and check for MD5 hash is valid.
+ * Until there is no hashes left, re-iterate over word list and increase the word limit for secret phrase to join them with space.
+ */
+
+const fs = require('fs')
+const readline = require('readline')
+const md5 = require('md5')
+
+const hashValues = [
+  'e4820b45d2277f3844eac66c903e84be', // Easy
+  '23170acc097c24edb98fc5488ab033fe', // Medium
+  '665e5bcb0c20062fe8abaaf4628bb154', // Hard,
+]
+// keeps the character set of anagram globally.
+let anagramChars = {}
+
+const main = (fileName, anagram) => {
+  console.time('found in')
+
+  const lineReader = readline.createInterface({
+    input: fs.createReadStream(fileName),
+  })
+  // parse anagram to characters
+  anagramChars = getCharsOfAnagram(anagram)
+  // keeps the valid words from wordlist file
+  const foundWords = []
+
+  lineReader
+    .on('line', (line) => {
+      // reading the file line by line and populate the array
+      if (isValidWord(line)) foundWords.push(line)
+    })
+    .on('close', () => {
+      // remove dublicates from the array and send them to real function
+      findPhrases([...new Set(foundWords)])
+    })
+}
+
+/**
+ * @param {string} anagram / given anagram of the phrase.
+ * @returns {object} / object of characters like map pairs.
+ */
+function getCharsOfAnagram(anagram) {
+  const obj = {}
+  const unspaceAnagram = anagram.replace(/ /g, '')
+
+  for (const char of unspaceAnagram) {
+    obj[char] = (obj[char] || 0) + 1
+  }
+  return Object.freeze(obj)
+}
+
+/**
+ * @param {string} word / word to check for whether matches characters with anagram characters.
+ * @returns {object} TODO
+ */
+function isValidWord(word = '', chars = {}) {
+  for (const c of word) {
+    chars[c] = (chars[c] || 0) + 1
+    if (!(chars[c] <= anagramChars[c])) return
+  }
+  return chars
+}
+
+function findPhrases(wordList) {
+  let wordCount = 1
+  while (hashValues.length > 0) {
+    console.log(`--------- Checking with ${wordCount} word combination --------`)
+    console.log(`===================================================`)
+
+    for (const phrase of listFilter(wordList, wordCount)) {
+      const md5String = md5(phrase).toString()
+      const index = hashValues.indexOf(md5String)
+
+      if (index > -1) {
+        showResult(hashValues[index], phrase)
+        hashValues.splice(index, 1)
+        if (hashValues.length === 0) break
+      }
+    }
+
+    wordCount++
+  }
+}
+
+function* listFilter(wordList, wordCount) {
+  const phrase = []
+  const chars = {}
+  const anagramLength = Object.values(anagramChars).reduce((a, c) => a + c)
+  let filteredWordList
+  let suitableLength
+
+  function* fn(index) {
+    if (index === wordCount - 1) {
+      suitableLength = anagramLength - Object.values(chars).reduce((a, c) => a + c, 0)
+      filteredWordList = wordList.filter(
+        (word) => word.length === suitableLength && isValidWord(word, { ...chars })
+      )
+
+      for (const word of filteredWordList) {
+        phrase[index] = word
+        return yield* permutation(phrase)
+      }
+    } else {
+      for (const word of wordList) {
+        if (phrase[index]) {
+          removeCharCounts(phrase[index], chars)
+          phrase[index] = ''
+        }
+
+        if (!addCharCounts(word, chars)) continue
+
+        phrase[index] = word
+        yield* fn(index + 1)
+      }
+    }
+  }
+  yield* fn(0)
+}
+
+function addCharCounts(word = '', chars = {}) {
+  const newChars = isValidWord(word, { ...chars })
+  if (newChars) return Object.assign(chars, newChars)
+}
+
+function removeCharCounts(word = '', chars = {}) {
+  for (const c of word) {
+    if (chars[c]) chars[c]--
+  }
+}
+
+function* permutation(arr, size = arr.length) {
+  const data = []
+  const used = []
+  const len = arr.length
+  yield* perm(0)
+
+  function* perm(index) {
+    if (index === size) return yield data.join(' ')
+
+    for (let i = 0; i < len; i++) {
+      if (!used[i]) {
+        used[i] = true
+        data[index] = arr[i]
+        yield* perm(index + 1)
+        used[i] = false
+      }
+    }
+  }
+}
+
+function showResult(hash, phrase) {
+  console.timeLog('found in', `\n\t ${hash} : ${phrase}`)
+  console.log(`---------------------------------------------------`)
+}
+
+main('wordlist', 'poultry outwits ants')
